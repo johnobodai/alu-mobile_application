9651947a4aac151770c5a951398f2480c9dd91a6
johnobodai <j.obodai@alustudent.com>
2024-02-10 16:21:41 +0000

    Style Scan and browse Buttons and icons

--- insight_lens/lib/home_screen.dart
+++ insight_lens/lib/home_screen.dart
@@ -18,15 +18,25 @@ class HomeScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Icon(
-                  Icons.image,
-                  size: 64, // Increased icon size
-                  color: Colors.blue, // Icon color
+                  Icons.cloud_upload_outlined,
+                  size: 200, // Decreased icon size
+                  color: Color.fromARGB(255, 48, 45, 45), // Icon color
                 ),
                 const SizedBox(height: 10), // Spacer
                 TextButton(
                   onPressed: () {
                     // Placeholder for browse action
                   },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        Colors.grey[300], // Grey background for Browse button
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 24), // Adjust button padding
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(30.0), // Rounded shape
+                    ),
+                  ),
                   child: const Text(
                     'Browse',
                     style: TextStyle(fontSize: 24), // Increased text size
@@ -34,26 +44,30 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 40), // Space between buttons
-            ElevatedButton.icon(
-              onPressed: () {
-                // Placeholder for scan action
-              },
-              icon: const Icon(
-                Icons.camera_alt,
-                size: 48, // Increased icon size
-              ),
-              label: const Text(
-                'Scan',
-                style: TextStyle(fontSize: 24), // Increased text size
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.blue, // Blue background for the Scan button
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16, horizontal: 24), // Adjust button padding
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0), // Rounded shape
+            const SizedBox(height: 120), // Space between buttons
+            SizedBox(
+              width: MediaQuery.of(context).size.width *
+                  0.7, // Scan button spans 70% of the width
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // Placeholder for scan action
+                },
+                icon: const Icon(
+                  Icons.camera_alt,
+                  size: 48, // Increased icon size
+                ),
+                label: const Text(
+                  'Scan',
+                  style: TextStyle(fontSize: 24), // Increased text size
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.blue, // Blue background for the Scan button
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16, horizontal: 24), // Adjust button padding
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0), // Rounded shape
+                  ),
                 ),
               ),
             ),
