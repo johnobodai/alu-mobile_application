cf3a63acceeef77029cfc6567b9c9656fd55ad96
Kakaymi10 <m.moussa@alustdent.com>
2024-03-11 21:13:39 +0200

    mmm

--- insight_lens/lib/main.dart
+++ insight_lens/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+
+  runApp(MyApp()); // Run your MyApp
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final providers = [EmailAuthProvider()];
+    final providers = [
+      EmailAuthProvider()
+    ]; // Check if EmailAuthProvider is imported correctly
 
     return MaterialApp(
       initialRoute:
@@ -15,6 +25,8 @@ class MyApp extends StatelessWidget {
           return SignInScreen(
             providers: providers,
             actions: [
+              // AuthStateChangeAction<SignedIn> should be defined properly
+              // Replace with appropriate logic or remove if not needed
               AuthStateChangeAction<SignedIn>((context, state) {
                 Navigator.pushReplacementNamed(context, '/profile');
               }),
@@ -25,6 +37,8 @@ class MyApp extends StatelessWidget {
           return ProfileScreen(
             providers: providers,
             actions: [
+              // SignedOutAction should be defined properly
+              // Replace with appropriate logic or remove if not needed
               SignedOutAction((context) {
                 Navigator.pushReplacementNamed(context, '/sign-in');
               }),
